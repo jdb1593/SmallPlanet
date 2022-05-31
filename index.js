9a26824501f3b05ac4bdd495e909957ee899a593
BlancPong <bu3820@naver.com>
2022-05-31 17:19:21 +0900

    모달 테스트

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function show () {
-    document.querySelector(".background").className = "background show";
-  }
-  
-  function close () { 
-    document.querySelector(".background").className = "background";
-  }
-  
-  document.querySelector(".show").addEventListener('click', show);
-  document.querySelector(".close").addEventListener('click', close);
+var target = document.querySelectorAll('.btn_open');
+var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
+var targetID;
+
+// 팝업 열기
+for(var i = 0; i < target.length; i++){
+  target[i].addEventListener('click', function(){
+    targetID = this.getAttribute('href');
+    document.querySelector(targetID).style.display = 'block';
+  });
+}
+
+// 팝업 닫기
+for(var j = 0; j < target.length; j++){
+  btnPopClose[j].addEventListener('click', function(){
+    this.parentNode.parentNode.style.display = 'none';
+  });
+}
