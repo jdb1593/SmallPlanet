159f83246d2a0336f2f2109ebc717c0a3c17e8e9
BlancPong <bu3820@naver.com>
2022-06-03 16:19:02 +0900

    수정2

---
+++ test.js
@@ -0,0 +1,98 @@
+var target = document.querySelectorAll('.btn_open');
+var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
+var targetID;
+
+
+//$('#pop_info').show();
+
+// 팝업 열기
+for(var i = 0; i < target.length; i++){
+  target[i].addEventListener('click', function(){
+    targetID = this.getAttribute('href');
+    document.querySelector(targetID).style.visibility = 'visible';
+    //document.querySelector(targetID).show();
+    //$('#pop_info').show();
+  });
+}
+
+// 팝업 닫기
+for(var j = 0; j < target.length; j++){
+  btnPopClose[j].addEventListener('click', function(){
+    this.parentNode.parentNode.style.visibility = 'hidden';
+    //this.parentNode.parentNode.hide();
+    //$('#pop_info').hide();
+  });
+}
+
+//slide-wrap
+var slideWrapper = document.getElementById('slider-wrap');
+//current slideIndexition
+var slideIndex = 0;
+//items
+var slides = document.querySelectorAll('#slider-wrap ul li');
+//number of slides
+var totalSlides = slides.length;
+//get the slide width
+var sliderWidth = slideWrapper.clientWidth;
+//set width of items
+slides.forEach(function (element) {
+    element.style.width = sliderWidth + 'px';
+})
+//set width to be 'x' times the number of slides
+var slider = document.querySelector('#slider-wrap ul#slider');
+slider.style.width = sliderWidth * totalSlides + 'px';
+
+// next, prev
+var nextBtn = document.getElementById('next');
+var prevBtn = document.getElementById('previous');
+var closeBtn = document.getElementById('close');
+nextBtn.addEventListener('click', function () {
+    plusSlides(1);
+});
+prevBtn.addEventListener('click', function () {
+    plusSlides(-1);
+});
+
+
+// hover
+slideWrapper.addEventListener('mouseover', function () {
+    this.classList.add('active');    
+});
+slideWrapper.addEventListener('mouseleave', function () {
+    this.classList.remove('active');   
+});
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlides(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    slideIndex = n;
+    if (slideIndex == -1) {
+        slideIndex = totalSlides - 1;
+    } else if (slideIndex === totalSlides) {
+        slideIndex = 0;
+    }
+
+    slider.style.left = -(sliderWidth * slideIndex) + 'px';
+    pagination();
+}
+
+//pagination
+slides.forEach(function () {
+    var li = document.createElement('li');
+    document.querySelector('#slider-pagination-wrap ul').appendChild(li);
+})
+
+function pagination() {
+    var dots = document.querySelectorAll('#slider-pagination-wrap ul li');
+    dots.forEach(function (element) {
+        element.classList.remove('active');
+    });
+    dots[slideIndex].classList.add('active');
+}
