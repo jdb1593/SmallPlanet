964c9f1114ad18be908672cf5816790b60caea98
BlancPong <bu3820@naver.com>
2022-06-15 15:02:26 +0900

    수정

--- SmallPlanet/src/main/webapp/script/login.js
+++ SmallPlanet/src/main/webapp/script/login.js
@@ -1,16 +1,4 @@
-function test() {
-    var p1 = document.getElementById('password1').value;
-    var p2 = document.getElementById('password2').value;
-    if( p1 != p2 ) {
-    alert("비밀번호가 일치 하지 않습니다");
-    return false;
-    } else{
-    alert("비밀번호가 일치합니다");
-    return true;
-    }
-
-}
-
+// 비밀번호 숨김
 $(document).ready(function(){
     $('.login-pw i').on('click',function(){
         $('input').toggleClass('active');
@@ -24,46 +12,72 @@ $(document).ready(function(){
     });
 });
 
-// 비밀번호 유효성 검사 문법
-$("#password").change(function(){
-    checkPassword($('#password').val(),$('id').val());
-});
-function checkPassword(password,id){
-    
-    if(!/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(password)){            
-        alert('숫자+영문자+특수문자 조합으로 8자리 이상 사용해야 합니다.');
-        $('#password').val('').focus();
-        return false;
-    }    
-    var checkNumber = password.search(/[0-9]/g);
-    var checkEnglish = password.search(/[a-z]/ig);
-    if(checkNumber <0 || checkEnglish <0){
-        alert("숫자와 영문자를 혼용하여야 합니다.");
-        $('#password').val('').focus();
-        return false;
-    }
-    if(/(\w)\1\1\1/.test(password)){
-        alert('같은 문자를 4번 이상 사용하실 수 없습니다.');
-        $('#password').val('').focus();
-        return false;
-    }
-        
-    if(password.search(id) > -1){
-        alert("비밀번호에 아이디가 포함되었습니다.");
-        $('#password').val('').focus();
-        return false;
-    }
-    let signupfrm = document.signUp;
-    let birthyear =  signupfrm.birthyear.value;
-    let birthmonth =  signupfrm.birthmonth.value;
-    let birthdate =  signupfrm.birthdate.value;
-    
-    let concat_birthday = CONCAT(birthyear,'-',birthmonth,'-',birthdate);
-    signupfrm.birthday.value = concat_birthday;
-    
-    signupfrm.submit();
-}
 
+
+// 비밀번호 확인
+$('.pw').focusout(function () {   
+    let pwd1 = $("#password_1").val();
+    let pwd2 = $("#password_2").val();
+    if ( pwd1 != '' && pwd2 == '' ) {
+        null;
+    } else if (pwd1 != "" || pwd2 != "") {
+        if (pwd1 == pwd2) {
+            $("#alert-success").css('display', 'block');
+            $("#alert-danger").css('display', 'none');
+        } else {
+            $("#alert-success").css('display', 'none');
+            $("#alert-danger").css('display', 'block');
+        }
+    }
+});
+
+
+// 비밀번호 유효성 검사 문법
+function checkPassword(){
+    // 비밀번호 확인
+    let pwd1 = $("#password_1").val();
+    let pwd2 = $("#password_2").val();
+    if(pwd1 !== pwd2) {
+        $('#password_1').val('').focus();
+        $('#password_2').val('').focus();
+        return false;
+    }
+    // 정규식
+    if(!/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(pwd1)){
+        if(!/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(pwd2)){  
+        $("#password-danger").css('display', 'block');
+        $("#password-danger3").css('display', 'none');
+        $('#password_1').val('').focus();
+        $('#password_2').val('').focus();
+        setTimeout(function() {
+            $("#password-danger").css('display', 'none');
+        },5000)
+        return false;
+        }
+    }    
+    // 생년월일 검사
+    let date = $("#year").val();
+    let date1 = $("#mon").val();
+    let date2 = $("#day").val();
+    
+    if(date < 1900 || date > 2022 ){
+        $("#password-danger2").css('display', 'block');
+        $('#year').val('').focus();
+        setTimeout(function() {
+            $("#password-danger2").css('display', 'none');
+        },5000)
+        return false;
+    }
+    if(/(\w)\1\1\1/.test(pwd1)){
+        if(/(\w)\1\1\1/.test(pwd2)){
+            $("#password-danger3").css('display', 'block');
+            $("#password-danger").css('display', 'none');
+            $('#password_1').val('').focus();
+            $('#password_2').val('').focus();
+            return false;
+        }
+    }
+}
 
 
 
